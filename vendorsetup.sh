b45fa44e037ddb71a7fa0f4ef0fe04aeb17a0b7c
DhirajSurvase <dms25121997@gmail.com>
2018-05-03 00:31:12 +0000

    panelli: cm >> lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-# Use this to add panelli to CM's lunch command menu
+# Use this to add panelli to lineage's lunch command menu
 for var in eng user userdebug; do
   add_lunch_combo lineage_panelli-$var
 done
